3d8689d14ab3ae309bddf2bd11db55a1cfdd4e46
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-06-30 20:31:32 +0300

    chore: list pods name in the default namespace

--- kuberrnetes/main.go
+++ kuberrnetes/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"os"
@@ -26,4 +29,31 @@ func main() {
 		fmt.Printf("error gettng kubernetes config %v\n", err)
 		os.Exit(1)
 	}
+	namespace := "dev011"
+	pods, err := ListPods(namespace, clentset)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	for _, pod := range pods.Items {
+		fmt.Printf("Pod name: %v\n", pod.Name)
+	}
+
+	var message string
+	if message == "" {
+		message = "Total pods in all namespaces"
+	} else {
+		message = fmt.Sprintf("Total pods in the namespace: %s", namespace)
+	}
+	fmt.Printf("%s %d\n", message, len(pods.Items))
+}
+
+func ListPods(namespace string, client kubernetes.Interface) (*v1.PodList, error) {
+	fmt.Println("get available pods for namespace")
+	pods, err := client.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		err = fmt.Errorf("error getting pods: %v\n", err)
+		return nil, err
+	}
+	return pods, nil
 }
